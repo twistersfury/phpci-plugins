cd0a8af89da9e3498941906a115bf4cdd8b8726c
Phoenix Osiris <blogger@twistersfury.com>
2016-08-28 23:20:41 -0400

    Debugging

--- src/Plugin/Grunt.php
+++ src/Plugin/Grunt.php
@@ -14,6 +14,8 @@
     class Grunt extends pcGrunt {
         use DirectoryCache;
 
+        public function __construct(\PHPCI\Builder $phpci, \PHPCI\Model\Build $build, array $options) { parent::__construct($phpci, $build, $options); }
+
         public function getDirectory() {
             return $this->directory . '/node_modules';
         }
